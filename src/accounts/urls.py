1e4ca53ca43c69b9bc62021c220198005af523cc
angelkrustev1 <angelkrustev07@gmail.com>
2026-03-29 00:46:06 +0200

    url fixes

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
