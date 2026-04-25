92c3d21d535555555ca378d12b3d866546c7e524
angelkrustev1 <angelkrustev07@gmail.com>
2026-04-25 19:56:36 +0300

    fix asgi

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -6,10 +6,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
 from chat.middleware import JwtAuthMiddleware
 from chat.routing import websocket_urlpatterns
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
