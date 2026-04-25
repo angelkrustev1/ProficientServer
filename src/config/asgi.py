67acb21083c8b96512cede6dad4023119dc75584
angelkrustev1 <angelkrustev07@gmail.com>
2026-04-25 19:37:34 +0300

    fixed asgi.py  for the deploy

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,13 +1,13 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from chat.middleware import JwtAuthMiddleware
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
